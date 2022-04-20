353d94f9232cc06e401801811d5bdb6ec56ddd4e
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-04-20 14:48:28 +0100

    card render

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./component/MyNav";
 import Welcome from "./component/Welcome";
+import LatestRelease from "./component/LatestRelease";
 
 function App() {
   return (
     <div>
       <MyNav />
       <Welcome />
+      <LatestRelease />
     </div>
   );
 }
---
+++ src/component/LatestRelease.jsx
@@ -0,0 +1,22 @@
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import fantasy from "../data/Fantasy.json";
+
+const LatestRelease = () => (
+  <Container>
+    <Row>
+      {fantasy.map((books) => (
+        <Col md={3}>
+          <Card key={books.asin}>
+            <Card.Img variant="top" src={books.img} />
+            <Card.Body>
+              <Card.Title>{books.title}</Card.Title>
+              <Card.Text>{books.category}</Card.Text>
+              <Button variant="primary">Price: {books.price} £</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  </Container>
+);
+export default LatestRelease;
